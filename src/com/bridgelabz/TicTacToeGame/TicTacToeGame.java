865be9a3c0a1fe4dcb56b785c2a3c39f3be23b81
shreyashdj <shreyashjadhav1799@gmail.com>
2022-10-03 19:57:13 +0530

    [Shreyash]: Added code to create method which assign letters to player and computer

--- src/com/bridgelabz/TicTacToeGame/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame/TicTacToeGame.java
@@ -1,31 +1,85 @@
 package com.bridgelabz.TicTacToeGame;
 
-import java.util.Arrays;
-
+import java.util.Scanner;
+/**
+ * @author SHREYASH JADHAV
+ */
 public class TicTacToeGame {
-	
-	public static void Gameboard (){
-		
-	char[] board = new char[10];		// initialized char array board of size 10
-	
 	/*
-	 * 	using for loop storing empty space at elements position
+	 *	created object of class Scanner
+	 * 	initialized char array gameBoard of size 10
+	 * 	taken char type variables player and computer
 	 */
+	Scanner scan = new Scanner(System.in);
+	static char[] gameBoard = new char[10];
+	static char player;
+	static char computer;
 	
-	for(int i=0; i<board.length; i++) {
-		board[i] = ' ';
+	/**
+	 * created initializeBoard method to Initialize Board with empty space
+	 */
+	public void initializeBoard () {		
+		/*
+		 * 	using for loop storing empty space at elements position
+		 */
+		for(int i = 1; i < gameBoard.length; i++) {
+			gameBoard[i] = ' ';
+		}
 	}
-	System.out.println(Arrays.toString(board));
-
 	
+	/**
+	 *	created printBoard method to print board
+	 */
+	public void printBoard() {
+		/*
+		 * 	created simple design of board and printing value of elements in it
+		 */
+		System.out.println("\n -------------");
+		System.out.println(" | " + gameBoard[1] + " | " + gameBoard[2] + " | " + gameBoard[3] + " |");
+		System.out.println(" -------------");
+		System.out.println(" | " + gameBoard[4] + " | " + gameBoard[5] + " | " + gameBoard[6] + " |");
+		System.out.println(" -------------");
+		System.out.println(" | " + gameBoard[7] + " | " + gameBoard[8] + " | " + gameBoard[9] + " |");
+		System.out.println(" -------------");
 	}
 	
+	/**
+	 *	created chooseLetter method to assign letters to player and computer
+	 */
+	public void chooseLetter() {
+		/*
+		 *	tacking user input for player letter
+		 *	if 'x' is entered then computer will get 'o'
+		 *	else computer get 'x'
+		 */
+		System.out.print("\n Enter the player letter x or o : ");
+		player = scan.next().charAt(0);
+		if (player == 'x') {
+			computer = 'o';
+		} else {
+			computer = 'x';
+		}
+		System.out.println("\n Player : " + player + " and Computer : " + computer);
+	}
+	
+	/**
+	 *	created main to run program
+	 */
 	public static void main(String[] args) {
-
-		System.out.println("Welcome to Tic-Tac-Toe Game");
+		/*
+		 *	printed welcome message
+		 *	created object of class TicTacToeGame
+		 *	calling methods initializeBoard, initializeBoard, chooseLetter
+		 */
+		System.out.println("\n Welcome to Tic-Tac-Toe Game");
 		
-		Gameboard();
+		TicTacToeGame game = new TicTacToeGame();
+		
+		game.initializeBoard();
+		game.initializeBoard();
+		game.chooseLetter();
+		
+		}
 		
 	}
 
-}
